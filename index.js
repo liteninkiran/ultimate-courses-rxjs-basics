873e5dbed78b076bc842259e6b75ea082d133c60
Kiran Anand <liteninkiran@hotmail.com>
2025-03-27 19:40:17 +0000

    Configuring your project to use RxJS

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 import { of } from 'rxjs';
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
 of('Hello', 'RxJS').subscribe(console.log);

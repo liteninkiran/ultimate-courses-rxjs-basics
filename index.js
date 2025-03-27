212908ee0d18f7a21b52e385e354b5bb3e257818
Kiran Anand <liteninkiran@hotmail.com>
2025-03-27 19:48:32 +0000

    Create your first observable

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import { of } from 'rxjs';
+import { Observable } from 'rxjs';
 
-of('Hello', 'RxJS').subscribe(console.log);
+const observer = {
+    next: value => console.log('Next', value),
+    error: error => console.log('Error', error),
+    complete: () => console.log('Complete'),
+}
+
+const observable = new Observable(subscriber => {
+    subscriber.next('Hello');
+    subscriber.next('World');
+    subscriber.complete();
+    subscriber.next('XXX');
+});
+
+observable.subscribe(observer);
+

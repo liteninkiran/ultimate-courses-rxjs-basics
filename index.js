6588848e380b765748c6d4470800bfb58f328c5c
Kiran Anand <liteninkiran@hotmail.com>
2025-03-30 14:58:19 +0100

    Transform streams using map, pluck, and mapTo

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import { interval, timer } from 'rxjs';
+import { of, fromEvent } from 'rxjs';
+import { map, mapTo, pluck } from 'rxjs/operators';
 
-// const source$ = interval(100);
-const source$ = timer(1000, 100);
+const source$ = of(1, 2, 3, 4, 5);
 
-// source$.subscribe(console.log);
+const sourcePiped$ = source$.pipe(
+    map(x => x * 10)
+);
+
+// sourcePiped$.subscribe(console.log);
+
+const keyup$ = fromEvent(document, 'keyup');
+const keycode$ = keyup$.pipe(map(event => event.code));
+const keycodePlucked$ = keyup$.pipe(pluck('code'));
+const keycodePressed$ = keyup$.pipe(mapTo('Key Pressed'));
+
+// keyup$.subscribe(console.log);
+// keycode$.subscribe(console.log);
+// keycodePlucked$.subscribe(console.log);
+// keycodePressed$.subscribe(console.log);
+
+

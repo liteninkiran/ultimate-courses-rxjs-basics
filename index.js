477ca8f754bb43701c0869c5122a4faeec15967e
Kiran Anand <liteninkiran@hotmail.com>
2025-03-30 12:02:43 +0100

    Emit items based on a duration with interval and timer

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-import { from } from 'rxjs';
+import { interval, timer } from 'rxjs';
 
-function* hello() {
-    yield 'Hello';
-    yield 'World';
-}
+// const source$ = interval(100);
+const source$ = timer(1000, 100);
 
-const observer = {
-    next: value => console.log(value),
-    error: error => console.log(error),
-    complete: () => console.log('Complete'),
-}
-
-const url = 'https://api.github.com/users/octocat'
-
-// const source = [1, 2, 3, 4, 5];
-// const source = 'Hello';
-// const source = fetch(url);
-const source = hello();
-const source$ = from(source);
-
-const sub = source$.subscribe(observer);
+// source$.subscribe(console.log);

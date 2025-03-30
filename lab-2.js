1e414b94b6afec7758412e5fd35b30b9ab60a244
Kiran Anand <liteninkiran@hotmail.com>
2025-03-30 16:58:51 +0100

    Lab 2. Create a countdown timer using fromEvent map and scan

---
+++ lab-2.js
@@ -0,0 +1,26 @@
+import { interval } from 'rxjs';
+import { scan, mapTo, filter } from 'rxjs/operators';
+
+// Elements
+const countdown = document.getElementById('countdown');
+
+// Streams
+const counter$ = interval(300);
+
+// Helper functions
+const setCountdown = value => countdown.innerHTML = value === 0 ? 'Liftoff' : value;
+const reducer = (acc, cur) => acc + cur;
+const filterer = value => value >= 0;
+
+/*
+ * Starting countdown example. In future lessons we will learn
+ * about how to seed our countdown, complete when the timer hits zero, 
+ * pause the countdown, and resume. More to come!
+ */
+const obs$ = counter$.pipe(
+        mapTo(-1),
+        scan(reducer, 10),
+        filter(filterer),
+    );
+
+const sub = obs$.subscribe(setCountdown);
